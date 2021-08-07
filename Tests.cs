452f065842d7123d89cf4a98f5d0a30147626103
Aditchfield <Awditchfield@gmail.com>
2021-08-07 17:57:30 +0100

    Add files via upload

---
+++ Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTestProject.Pages;
+
+namespace SeleniumTestProject
+{
+    [TestClass]
+    public class Tests
+    {
+        IWebDriver driver;
+        Homepage homepage;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("http://cust.themodernmilkman.co.uk/");
+            homepage = new Homepage();
+        }
+        [TestMethod]
+        public void SignUpTest()
+        {
+            var signupPage = homepage.EnterPostcode(driver);
+            signupPage.SubmitForm(driver);
+        }
+        [TestMethod]
+        public void SignInTest() 
+        {
+            var SigninPage = homepage.ClickSignIn(driver);
+            SigninPage.SignIn(driver);
+        }
+        [TestCleanup]
+        public void Shutdown()
+        {
+            driver.Close();
+        }
+    }
+}
